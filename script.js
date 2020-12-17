948d865ff4c6a62d8df4c476d75061149f70abdc
Leo Thomas Monteiro <55034110+anotherleo@users.noreply.github.com>
2020-12-17 02:45:55 -0300

    js tinkering.

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 function showNotes() {
+    document.getElementById("authorNotes").style.display="block";
+}
+
+/*
+    document.getElementById("notes").onclick = hideNotes() {hideNotes(document.getElementById("authorNotes").style.display="none")}
+}
+*/
+
+
+/*
+while (document.getElementById("authorNotes").style.display="block") {
+*/
+        
+/*
+function showNotes() {
     var showSection = document.getElementById("authorNotes");
     if (showSection.style.display === "none") {
         showSection.style.display = "block";
     } else {
-        showSection.style.display === "block";
         showSection.style.display = "none";
     }
-  }
+}
+*/
